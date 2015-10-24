4b7c38095b298a7973a4ca84bd4d05582dae58b9
aguzmanc <spacerockganimedes@gmail.com>
2015-10-24 11:56:06 -0400

    pre release

---
+++ config/bancos/configuracion.js
@@ -0,0 +1,52 @@
+bancos = [];
+
+bancos.push({
+  nombre : 'Banco Unión',
+  imagenBanco : 'banco-union/banco.png',
+  imagenTarjeta : 'banco-union/tarjeta.png'
+});
+
+/**
+ * Estos son bancos de ejemplo.
+ * Así es como se deben añadir nuevos bancos:
+ * imagenBanco, es una variable que contiene, en un string,
+ * el nombre del archivo dentro de esta carpeta,
+ * de la imagen de la cabecera del banco.
+ *
+ * imagenTargeta, es una variable que contiene, en un string,
+ * el nombre del archivo dentro de esta carpeta,
+ * de la imagen de la tarjeta usada por este banco.
+ *
+ * nombre, es una variable que contiene el nombre del banco
+ * en un string.
+ */
+
+bancos.push({
+  nombre : 'Kitten Bank',
+  imagenBanco : 'kitten/banco.png',
+  imagenTarjeta : 'kitten/tarjeta.png'
+});
+
+bancos.push({
+  nombre : 'Banco 0',
+  imagenBanco : 'banco0/banco.png',
+  imagenTarjeta : 'banco0/tarjeta.png'
+});
+
+bancos.push({
+  nombre : 'Banco 1',
+  imagenBanco : 'banco1/banco.png',
+  imagenTarjeta : 'banco1/tarjeta.png'
+});
+
+bancos.push({
+  nombre : 'Banco 2',
+  imagenBanco : 'banco2/banco.png',
+  imagenTarjeta : 'banco2/tarjeta.png'
+});
+
+bancos.push({
+  nombre : 'Banco 3',
+  imagenBanco : 'banco3/banco.png',
+  imagenTarjeta : 'banco3/tarjeta.png'
+});
